dec420c809d5d4be3fe2292d813c957d13163851
Stephen Kell <srk31@cl.cam.ac.uk>
2014-10-10 16:07:10 +0000

    Do something sensible on systems without /proc/<pid>/sched: use /proc/<pid>/schedstat instead.

--- dumpsched.c
+++ dumpsched.c
@@ -6,6 +6,29 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdarg.h>
+#include <elf.h>
+#include <sys/auxv.h>
+#include <link.h>          /* for ElfW(...) macro */
+#include <linux/auxvec.h>  /* For AT_xxx definitions */
+
+static char *getprogname(void)
+{
+	/* Dig into the auxv. We could find it within our address space,
+	 * but that's hard work. Read it, one record at a time, from /proc. */
+	ElfW(auxv_t) buf;
+	FILE *auxv = fopen("/proc/self/auxv", "r");
+	if (!auxv) return "(no auxv)";
+
+	size_t nread;
+	while (0 != (nread = fread(&buf, sizeof buf, 1, auxv)))
+	{
+		if (buf.a_type == AT_EXECFN)
+		{
+			return basename((char*) buf.a_un.a_val);
+		}
+	}
+	return "(auxv but no AT_EXECFN)";
+}
 
 static void print_exit_summary(void)
 {
@@ -36,7 +59,33 @@ static void print_exit_summary(void)
 
 		fclose(sched);
 	}
-	else fprintf(stderr, "Couldn't read from sched!\n");
+	else
+	{
+		FILE *schedstat = fopen("/proc/self/schedstat", "r");
+		
+		if (schedstat)
+		{
+			fprintf(stderr, "%s (%d)\n", getprogname(), getpid());
+			/* https://www.kernel.org/doc/Documentation/scheduler/sched-stats.txt */
+			long int t_cpu, t_runqueue, n_slices_on_this_cpu;
+			int ret = fscanf(schedstat, "%ld %ld %ld\n", 
+				&t_cpu, &t_runqueue, &n_slices_on_this_cpu);
+			if (ret == 3)
+			{
+				fprintf(stderr, "schedstat total time: %ld\n", t_cpu + t_runqueue);
+			}
+			else
+			{
+				fprintf(stderr, "Could not parse schedstat\n");
+			}
+
+		}
+		else
+		{
+			fprintf(stderr, "Couldn't read from sched!\n");
+		}
+
+	}
 	fflush(stderr);
 }
 
